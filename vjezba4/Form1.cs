03d9fb1496795a7b87026365bfff92e429d6e80b
domagoj.bitanga <domagoj.bitanga@KABINFB-05>
2025-11-24 19:40:01 +0100

    na satu

--- vjezba4/Form1.cs
+++ vjezba4/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,49 @@ namespace vjezba4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string linija;
+            StreamReader sr = new StreamReader("student.txt", true);
+            linija = sr.ReadLine();
+            List<string> lstStr = new List<string>();
+            while (linija != null)
+            {
+                linija = linija.Replace("|", " ");
+                lstStr.Add(linija);
+                linija = sr.ReadLine();
+            }
+            listBox1.DataSource = lstStr;
+
+            sr.Close();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string linija;
+            linija = textBox1.Text + "|" + textBox2.Text;
+            StreamWriter sw = new StreamWriter("student.txt", true);
+            sw.WriteLine(linija);
+            sw.Close();
+            textBox2.Text = "";
+            textBox1.Text = "";
+            MessageBox.Show("Podaci su spremljeni!");
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
